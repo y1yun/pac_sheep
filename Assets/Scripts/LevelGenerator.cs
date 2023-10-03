e260ad5c72ac429efc62b92129c50b00f749892b
Penghuan Zhang <70063154+y1yun@users.noreply.github.com>
2023-10-04 10:23:13 +1100

    map generated but without rotations

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject tileMap;
+    public GameObject outsideCorner;
+    public GameObject outsideWall;
+    public GameObject insideCornor;
+    public GameObject insideWall;
+    public GameObject standardPellet;
+    public GameObject powerPellet;
+    public GameObject tJunction;
 
 
     int[,] levelMap =
@@ -31,11 +38,69 @@ public class LevelGenerator : MonoBehaviour
     void Start()
     {
         tileMap.SetActive(false);
+
+        Generate();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Generate()
     {
-        
+        //levelMap.GetLength(0) gives you the length (the number of rows)
+        //levelMap.GetLength(1) gives you the length (the number of columns) 
+        for (int col =0; col < levelMap.GetLength(1); col++)
+        {
+            for (int row=0; row < levelMap.GetLength(0); row++)
+            {
+                int tileType = levelMap[row, col];
+                Vector3 tilePosition = new Vector3(col, -row, 0);
+
+                GameObject tileAsset = ChooseType(tileType);
+
+                if (tileAsset != null) //avoid case 0
+                Instantiate(tileAsset, tilePosition, Quaternion.identity);
+                //Quaternion.identity indicates no initial rotation
+            }
+        }
+    }
+
+    GameObject ChooseType(int tileType)
+    {
+// 0 – Empty (do not put anything here, no sprite needed)
+// 1 - Outside corner (double lined corner piece in original game)
+// 2 - Outside wall (double line in original game)
+// 3 - Inside corner (single lined corner piece in original game)
+// 4 - Inside wall (single line in original game)
+// 5 – An empty space with a Standard pellet (see Visual Assets above)
+// 6 – An empty space with a Power pellet (see Visual Assets above)
+// 7 - A t junction piece for connecting with adjoining regions
+        switch(tileType)
+        {
+            case 0:
+            return null;
+
+            case 1:
+            return outsideCorner;
+
+            case 2:
+            return outsideWall;
+
+            case 3:
+            return insideCornor;
+
+            case 4:
+            return insideWall;
+
+            case 5:
+            return standardPellet;
+
+            case 6:
+            return powerPellet;
+
+            case 7: 
+            return tJunction;
+
+            default:
+            return null; //avoid null exceptions
+
+        }
     }
 }
